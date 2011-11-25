9e89dfcb7b9d52223e5ccd4c0d0850c2c36cf6d1
Will Haack <wmh1993@gmail.com>
2011-11-25 13:03:56 -0500

    yo

--- playlist.js
+++ playlist.js
@@ -1,10 +1,8 @@
-window.onload = init();
-
+window.onload = init;
 function init(){
     var button = document.getElementById("addButton");
-    button.onclick = handleButtonClick();
+    button.onclick = handleButtonClick;
 }
-
 function handleButtonClick() {
     alert("Button was clicked!");
 }
