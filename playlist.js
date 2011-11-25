1e4f67cd5daf684d855d40208ad6347f39fb3dac
Will Haack <wmh1993@gmail.com>
2011-11-25 13:27:12 -0500

    yo

--- playlist.js
+++ playlist.js
@@ -2,6 +2,7 @@ window.onload = init;
 function init(){
     var button = document.getElementById("addButton");
     button.onclick = handleButtonClick;
+    loadPlaylist();
 }
 function handleButtonClick() {
     var textInput = document.getElementById("songTextInput");
@@ -10,5 +11,6 @@ function handleButtonClick() {
     li.innerHTML = songName;
     var ul = document.getElementById("playlist");
     ul.appendChild(li);
+    save(songName);
 }
 
